25370868138c3b410de5aea629c50b6030730563
Ian Huang <97159015+PypySplash@users.noreply.github.com>
2026-08-07 02:28:16 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> memo(n, -1);
+        // dfs(i) 表示从 nums[0] 到 nums[i] 最多能偷多少
+        auto dfs = [&] (this auto&& dfs, int i) -> int {
+            if (i < 0) {
+                return 0;
+            }
+            if (memo[i] != -1) { // 之前计算过
+                return memo[i];
+            }
+            // 選 or 不選
+            return memo[i] = max(dfs(i-1), dfs(i-2) + nums[i]);
+        };
+        return dfs(n-1);
+    }
+};
