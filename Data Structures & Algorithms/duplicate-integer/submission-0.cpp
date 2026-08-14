407520712ea6ebfef25acd8cba69a1614a187aeb
Ian Huang <97159015+PypySplash@users.noreply.github.com>
2026-08-14 16:54:44 -0700

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for (int x : nums) {
+            if (st.contains(x)) {
+                return true;
+            }
+            st.insert(x);
+        }
+        return false;
+    }
+};
