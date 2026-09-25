7d29697944a703618f10ecd0c84d0a32d83e0fce
Ian Huang <97159015+PypySplash@users.noreply.github.com>
2026-09-25 05:55:59 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,33 @@
+# include <stack>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.emplace(c);
+            }
+            else if (c == ')') {
+                if (!st.empty() && st.top() == '(') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (!st.empty() && st.top() == '{') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else { // if (c == ']')
+                if (!st.empty() && st.top() == '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty() ? true : false;
+    }
+};
